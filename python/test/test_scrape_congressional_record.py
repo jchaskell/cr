15c38a4665a092bb101903c35c26f556f5a2779d
Jennifer Haskell <jhaskell@gmail.com>
2019-01-12 22:04:49 -0800

    add dummy test

--- python/test/test_scrape_congressional_record.py
+++ python/test/test_scrape_congressional_record.py
@@ -1,1 +1,11 @@
-# TODO: Write tests
+"""This module tests scrapeCR"""
+
+import unittest
+
+class TestScrapeCR(unittest.TestCase):
+    # TODO: add tests
+    def test_dummy(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+    unittest.main()
