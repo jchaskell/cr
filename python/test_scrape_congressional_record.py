bc7c9d29a0c1e052fce42420cda1e3c91f701d28
Jennifer Haskell <jhaskell@gmail.com>
2019-01-12 22:06:13 -0800

    move files around some more

---
+++ python/test_scrape_congressional_record.py
@@ -0,0 +1,11 @@
+"""This module tests scrapeCR"""
+
+import unittest
+
+class TestScrapeCR(unittest.TestCase):
+    # TODO: add tests
+    def test_dummy(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+    unittest.main()
