b4bb47dd58cf6936288c30b57fe6953dbc97b9a2
Jennifer Haskell <jhaskell@gmail.com>
2019-01-22 23:04:24 -0800

    write first set of tests for CRWriter (tests that do not involve requests)

--- python/test/test_scrape_congressional_record.py
+++ python/test/test_scrape_congressional_record.py
@@ -1,11 +1,41 @@
 """This module tests scrapeCR"""
 
+import re
 import unittest
+from datetime import datetime
+from cr.scrape_congressional_record import CRWriter
 
-class TestScrapeCR(unittest.TestCase):
-    # TODO: add tests
-    def test_dummy(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+startdate = "01-01-2010"
+enddate = "01-02-2010"
+tmp_directory = "temp"
+
+#class CRScraperTest(unittest.TestCase):
+    # TODO: write these tests
+
+class CRWriterTest(unittest.TestCase):
+    expected_dates = [datetime.strptime(startdate, "%m-%d-%Y").date(),
+            datetime.strptime(enddate, "%m-%d-%Y").date()]
+    expected_links = ["https://www.congress.gov/congressional-record/2010/01/01/senate-section",
+            "https://www.congress.gov/congressional-record/2010/01/02/senate-section"]
+    expected_filenames = ["temp/S2010-01-01.txt", "temp/S2010-01-02.txt"]
+
+    def setUp(self):
+        self.test_writer = CRWriter("s", tmp_directory, startdate, enddate)
+        self.test_writer_house = CRWriter("h", tmp_directory, startdate, enddate)
+
+    def test_daterange(self):
+        self.assertEqual([d for d in self.test_writer.daterange()], self.expected_dates)
+
+    def test_create_links(self):
+        self.assertEqual(self.test_writer.create_links(), self.expected_links)
+        self.assertEqual(self.test_writer_house.create_links(), [re.sub("senate", "house", link) for link in self.expected_links])
+
+    def test_create_filenames(self):
+        self.assertEqual(self.test_writer.create_filenames(), self.expected_filenames)
+        self.assertEqual(self.test_writer_house.create_filenames(), [re.sub("S", "H", f) for f in self.expected_filenames])
+
+    #def test_run(self):
+        # TODO: finish this test
 
 if __name__ == '__main__':
     unittest.main()
